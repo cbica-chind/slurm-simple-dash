f9a6b37187ca908c143b2c60f13bea11e173539d
David Chin <139799987+cbica-chind@users.noreply.github.com>
2025-11-06 16:30:24 -0500

    handle gres

--- slurm-simple-dash.py
+++ slurm-simple-dash.py
@@ -36,6 +36,8 @@ def get_sinfo(partition='all'):
     for s in sinfo_dict['sinfo']:
         nthreads = s['sockets']['maximum'] * s['cores']['maximum'] * s['threads']['maximum']
         load = float(s['cpus']['load']['maximum']) / 100.
+        gres_total = s['gres']['total']
+        gres_used = s['gres']['used']
 
         if _DEBUG and _VERBOSE:
             print(f's.keys() = {s.keys()}')
@@ -47,7 +49,12 @@ def get_sinfo(partition='all'):
             print()
 
         fractional_load = load / float(nthreads) * 100.
-        node_loads[s['nodes']['nodes'][0]] = {'nthreads': nthreads, 'allocated': s['cpus']['allocated'], 'load': load, 'fractional load': fractional_load}
+        node_loads[s['nodes']['nodes'][0]] = {'nthreads': nthreads,
+                                              'allocated': s['cpus']['allocated'],
+                                              'load': load,
+                                              'fractional load': fractional_load,
+                                              'gres total': gres_total,
+                                              'gres used': gres_used}
 
     if _DEBUG and _VERBOSE:
         for k,v in node_loads.items():
@@ -65,12 +72,18 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-d', '--debug', action='store_true', help='debugging output')
     parser.add_argument('-v', '--verbose', action='store_true', help='verbose output')
+    parser.add_argument('-p', '--partition', help='partition', default='all')
     args = parser.parse_args()
 
     _DEBUG = args.debug
     _VERBOSE = args.verbose
 
-    get_sinfo()
+    # pandas display options
+    pd.set_option('display.max_rows', None)
+    pd.set_option('display.max_columns', None)
+    pd.set_option('display.width', None)
+
+    get_sinfo(args.partition)
 
 
 if __name__ == '__main__':
